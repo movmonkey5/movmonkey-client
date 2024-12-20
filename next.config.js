819a4d62bee1292ae8feeeef27965d5a3b8b00df
Al -Adnan Mehedi <36293175+adnanmehedi2462@users.noreply.github.com>
2024-12-20 14:33:24 +0600

    solved bug v2 driver and cleaning

--- next.config.js
+++ next.config.js
@@ -5,18 +5,10 @@ const nextConfig = {
     domains: [
       "stagingmovmonkeyv1.pythonanywhere.com",
       "movmonkey.stringify.tech",
-<<<<<<< HEAD
       "backend.movmonkey.com",
       "127.0.0.1", // Add your local domain
-=======
-      'backend.movmonkey.com',
->>>>>>> 3926f1b0ceda5b1d58e58de5e4d2666a2233b199
     ],
   },
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3926f1b0ceda5b1d58e58de5e4d2666a2233b199
 module.exports = nextConfig;
