4a627fea2faa48e70cf9faad0aaaac07894b2ffd
Al Adnan Mehedi <adnan@fairpattern.com>
2025-08-23 21:47:43 +0600

    Fix lockfile mismatch for Netlify

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: [
-      "stagingmovmonkeyv1.pythonanywhere.com",
+      "",
       "movmonkey.stringify.tech",
       "backend.movmonkey.com",
       "127.0.0.1", // Add your local domain
