f40a4d5e1c695c2c7162c55e649764c836e603e4
Al Adnan Mehedi <adnan@fairpattern.com>
2025-09-01 18:19:03 +0600

    ok

--- next.config.js
+++ next.config.js
@@ -2,29 +2,22 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [
-      "",
-      "movmonkey.stringify.tech",
-      "testit.movmonkey.com",
-      "127.0.0.1", // Add your local domain
-      'images.unsplash.com',
-    ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "http",
+        hostname: "testit.movmonkey.com",
+        pathname: "/media/blog_images/**",
       },
       {
-        protocol: 'http',
-        hostname: 'testit.movmonkey.com',
+        protocol: "https",
+        hostname: "testit.movmonkey.com",
+        pathname: "/media/blog_images/**",
       },
       {
-        protocol: 'https',
-        hostname: 'testit.movmonkey.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
-    // Add these settings for better Vercel compatibility
-    unoptimized: false, // Keep optimization enabled
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
